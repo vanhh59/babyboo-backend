57b7c50618547ebc04359995788d3b16d4fe0a7b
vanhh59 <vanhhh59@gmail.com>
2024-06-18 13:32:29 +0700

    change port

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 connectDB();
 
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-  
+
 app.use("/api/users", userRoutes);
 // app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
