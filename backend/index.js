9112f729353c37e3ab51e3ff6f85ed94fb094d0b
vanhh59 <vanhhh59@gmail.com>
2024-06-21 13:36:33 +0700

    install cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 // Utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -22,6 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
