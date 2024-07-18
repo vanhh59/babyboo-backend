bfbac16437f8e8f6f23781ce045108b399a9e8a5
Tien <thuytienln238@gmail.com>
2024-07-19 00:21:08 +0700

    Update model and limit get in productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -122,7 +122,6 @@ const fetchAllProducts = asyncHandler(async (req, res) => {
   try {
     const products = await Product.find({})
       .populate("category")
-      .limit(12)
       .sort({ createAt: -1 });
 
     res.json(products);
